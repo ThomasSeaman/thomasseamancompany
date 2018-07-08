250d6c5f2cbc573637eedf59df65b8f622728017
Thomas Seaman <thomasseaman@Thomas-School-MacBook-Pro.local>
2018-07-07 18:05:47 -0700

    push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import { Col, Row } from 'react-materialize';
+import Footer from './Components/Footer/Footer';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1 className="App-title">Thomas Seaman Company</h1>
+      <Row>
+        <Col s={12} className='title'>Thomas Seaman Company</Col>
+      </Row>
+      <Row>
+      <Col s={12} className='companyDescription'>Orange Country Judical Reciever</Col>
+    </Row>
+    <Row>
+      <Col s={12} className='companyDescription'>Cases</Col>
+    </Row>
+    <Row>
+      <Col s={4} className='companyDescription'>Construction</Col>
+      <Col s={4} className='companyDescription'>Rents and Profits</Col>
+      <Col s={4} className='companyDescription'>Operating Companies</Col>
+    </Row>
+    <Row>
+      <Col s={4} className='companyDescription'>Assisted Living</Col>
+      <Col s={4} className='companyDescription'>Regulatory</Col>
+      <Col s={4} className='companyDescription'>Other Notable Cases</Col>
+    </Row>
+    <Row>
+      <Col s={4} className='companyDescription'>About</Col>
+      <Col s={4} className='companyDescription'>Contact</Col>
+      <Col s={4} className='companyDescription'>Team</Col>
+    </Row>
+    <Footer></Footer>
       </div>
-      
     );
   }
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => (
+<div><p>Footer</p></div>
+);
+
+export default Footer;
