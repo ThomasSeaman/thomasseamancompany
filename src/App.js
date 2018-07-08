b1304d81a7c85aa086cb0cb4d3f09d1611b7fdfe
Thomas Seaman <thomasseaman@Thomas-School-MacBook-Pro.local>
2018-07-08 11:10:44 -0700

    google fonts

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <Nav></Nav>
         <Title></Title>
         <CompanyDescription></CompanyDescription>
